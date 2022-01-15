364667deb063047c6adfbd0de3543869955b15a6
amduraes <andremdpereira@gmail.com>
2022-01-15 20:47:55 +0000

    infinite scroll

--- src/hooks/useGetMovies.js
+++ src/hooks/useGetMovies.js
@@ -1,18 +1,31 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const useGetMovies = (page, size) => {
-	const [movies, setMovies] = useState('')
+const useGetMovies = (pageNumber, pageSize) => {
+	const [allMovieData, setAllMovieData] = useState([]);
+	const [movies, setMovies] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [hasError, setHasError] = useState(false);
+	const [hasMore, setHasMore] = useState(false);
 
 	useEffect(() => {
+		setIsLoading(true);
+		setHasError(false);
 		axios({
-			method: 'GET',
-			url: 'http://movie-challenge-api-xpand.azurewebsites.net/api/movies',
-			params: { page, size }
-		}).then((res) => console.log(res.data.content))
-	}, [page, size])
+			method: "GET",
+			url: "http://movie-challenge-api-xpand.azurewebsites.net/api/movies",
+			params: { page: pageNumber, size: pageSize },
+		})
+			.then((res) => {
+				setAllMovieData(res.data);
+				setMovies((prev) => [...prev, ...res.data.content]);
+				setHasMore(res.data.content.length > 0);
+				setIsLoading(false);
+			})
+			.catch(() => setHasError(true));
+	}, [pageNumber, pageSize]);
 
-	return movies;
-}
+	return { allMovieData, movies, isLoading, hasError, hasMore };
+};
 
-export default useGetMovies
+export default useGetMovies;
